5f6dccac413100d0b53d39c854a1f8d3781bd9be
Jorge Ulises Useche Cuellar <naturalmentejorge@gmail.com>
2016-10-14 01:10:36 -0500

    send post other forms

--- registrarComentarios.js
+++ registrarComentarios.js
@@ -6,7 +6,7 @@
 // @grant       none
 // ==/UserScript==
 
-var _abrirEnlaces = 'no'
+window._formularios = new Array()
 
 var _miscript = function() {
   //Valido solo para ese dominio en la página "My Personal Page" con el Widget "My Artifacts (Tracker V5) [AS]"
@@ -15,9 +15,8 @@ var _miscript = function() {
     var comentar = prompt('Desea realizar comentarios? si/no')
     console.log(comentar)
 
-    _abrirEnlaces = prompt('Abrir enlaces después de comentar? si/no')
-
     if (comentar === 'si') {
+      window._abrirEnlaces = prompt('Abrir enlaces después de comentar? si/no')
 
       jQuery('#widget_plugin_tracker_myartifacts-0-ajax > div > ul > li > a')
         .each(function(i) {
@@ -65,46 +64,18 @@ function peticionGET(url) {
       var final = respuestaHTML.indexOf('</form>', inicio)
       var formulario = respuestaHTML.substring(inicio, final)
       formulario = jQuery(formulario)
+        // https://css-tricks.com/snippets/html/form-submission-new-window/
+      formulario.attr('target', '_blank')
 
       var titulo = formulario.find('.tracker-hierarchy').text()
       var comentario = prompt('Escriba el comentario para: ' + titulo)
 
       formulario.find('#tracker_followup_comment_new').val(comentario)
 
-      peticionPOST(url + '&func=artifact-update', formulario)
+
+      document.body.append(formulario)
+
+      //formulario.submit()
+      window._formularios.push(formulario)
     });
-}
-
-function peticionPOST(url, form) {
-  jQuery.ajax({
-      contentType: 'multipart/form-data',
-      method: 'POST',
-      url: url,
-      data: form.serialize(),
-      xhr: function() { //http://stackoverflow.com/questions/3372962/can-i-remove-the-x-requested-with-header-from-ajax-requests
-        // Get new xhr object using default factory
-        var xhr = jQuery.ajaxSettings.xhr();
-        // Copy the browser's native setRequestHeader method
-        var setRequestHeader = xhr.setRequestHeader;
-        // Replace with a wrapper
-        xhr.setRequestHeader = function(name, value) {
-            // Ignore the X-Requested-With header
-            if (name == 'X-Requested-With') return;
-            // Otherwise call the native setRequestHeader method
-            // Note: setRequestHeader requires its 'this' to be the xhr object,
-            // which is what 'this' is here when executed.
-            setRequestHeader.call(this, name, value);
-          }
-          // pass it on to jQuery
-        return xhr;
-      }
-    })
-    .done(function(msg) {
-      console.log('Éxito:', msg)
-      if (_abrirEnlaces === 'si') {
-
-        window.open(url, '_blank')
-
-      }
-    })
 }
